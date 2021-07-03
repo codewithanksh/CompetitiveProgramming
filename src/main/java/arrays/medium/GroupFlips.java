bde283780f44222ab72de50e6c7c8aaaa925b74f
Ankush <ankushsharma209@gmail.com>
2021-07-03 13:34:49 +0530

    problem in progress - efficient solution solved

--- src/main/java/arrays/medium/GroupFlips.java
+++ src/main/java/arrays/medium/GroupFlips.java
@@ -1,5 +1,7 @@
 package arrays.medium;
 
+import java.util.Arrays;
+
 /**
  * Given a binary array,
  * we need to find the minimum of number of group flips to make all array elements same.
@@ -9,7 +11,7 @@ public class GroupFlips {
 
     static int minimumGroupFlips(int[] arr) {
 
-        if(arr.length < 2)
+        if (arr.length < 2)
             return -1;
 
         int count = 0;
@@ -52,7 +54,7 @@ public class GroupFlips {
 
                 int start = i;
 
-                while (i < arr.length && arr[i] == 0 )
+                while (i < arr.length && arr[i] == 0)
                     i++;
 
                 int end = i - 1;
@@ -87,6 +89,59 @@ public class GroupFlips {
 
     }
 
+    static int minimumGroupFlips_(int[] arr) {
+
+        if (arr.length < 2)
+            return -1;
+
+        int firstGroup = arr[0] == 0 ? 0 : 1;
+        int secondGroup = firstGroup == 0 ? 1 : 0;
+
+        int counter = 0, flips = 0;
+
+//        while (counter < arr.length) {
+//
+//            boolean flag = false;
+//            int start = counter;
+//
+//            while (counter < arr.length && arr[counter] == secondGroup) {
+//                flag = true;
+//                counter++;
+//            }
+//
+//            if (flag) {
+//                flips++;
+//                System.out.println("Input Array is " + Arrays.toString(arr) + " ,Start Index for second group is " + start + " && ending index is " + (counter - 1));
+//            }
+//
+//            counter++;
+//        }
+//
+//        if (flips == 0)
+//            System.out.println("Input Array is " + Arrays.toString(arr) + " No Second group found in the array");
+
+        for (int i = 1; i < arr.length; i++) {
+
+            // Condition indicates starting of a new second group or ending of an existing second group
+            if (arr[i] != arr[i - 1]) {
+
+                //Begining of a second group when current element is not equal to the first element
+                if (arr[i] != arr[0])
+                    System.out.println("starting from "+i);
+                else {
+                    System.out.println("ending at " + (i - 1));
+                    flips++;
+                }
+
+            }
+
+            if(arr[arr.length-1] != arr[0]) {
+                System.out.println("ending at " + (arr.length - 1));
+                flips++;
+            }
+        }
+        return flips;
+    }
 
     public static void main(String[] args) {
         int[] arr = {0, 1, 1, 1, 0};
@@ -100,5 +155,17 @@ public class GroupFlips {
 
         arr = new int[]{1, 0};
         minimumGroupFlips(arr);
+
+        arr = new int[]{0, 1, 1, 1, 0};
+        minimumGroupFlips_(arr);
+
+        arr = new int[]{1, 1, 0, 0, 0, 1};
+        minimumGroupFlips_(arr);
+
+        arr = new int[]{1, 1, 1};
+        minimumGroupFlips_(arr);
+
+        arr = new int[]{1, 0};
+        minimumGroupFlips_(arr);
     }
 }
